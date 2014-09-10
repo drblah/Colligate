d6a36368074743adfb6e43e67983bf00e2eadfe3
Sebastian Damsgaard <theblahblah2001@gmail.com>
2014-09-10 18:45:14 +0200

    Clean up menu
    Cleaned out old menu items used for testing.

--- Colligate.rb
+++ Colligate.rb
@@ -14,12 +14,7 @@ dbhandeler = DBmanager.new()
 while true
 	puts("\nPlease choose an activity:\n\n")
 
-	puts("1. Download newest data from the server\n")
-	puts("2. Load auctions into memory\n")
-	puts("3. Store auctions into the database")
-	puts("4. Download, Load and store")
-	puts("5. AutoMode")
-	puts("6. Test")
+	puts("1. Download, Load and store")
 	puts("0. Exit program\n")
 	
 
@@ -34,41 +29,8 @@ while true
 	end
 
 	case input
+
 	when "1"
-		dataInfo = downloader.getauctionURL
-
-		@lastModified = dataInfo[1]
-
-		downloader.downloadAuctionJSON(dataInfo[0])
-
-	when "2"
-
-		dbhandeler.readAuctionJSON
-
-	when "3"
-
-		dbhandeler.writeAuctionsToDB(dbhandeler.readAuctionJSON,@lastModified)
-
-	when "4"
-
-		dataInfo = downloader.getauctionURL
-
-		@lastModified = dataInfo[1]
-
-		downloader.downloadAuctionJSON(dataInfo[0])
-
-
-		success = dbhandeler.writeAuctionsToDB(dbhandeler.readAuctionJSON,@lastModified)
-
-		if success
-			success = dbhandeler.moveoldtolog(@lastModified)	
-		end
-
-		if success
-			dbhandeler.deleteold(@lastModified)
-		end
-
-	when "5"
 
 		oldLastModified = 0
 
@@ -152,28 +114,6 @@ while true
 			sleep(300)
 
 		end
-
-	when "6"
-		
-		
-		missingItems = dbhandeler.itemsNotInDB
-
-		puts missingItems.length
-
-		itemJSON = Array.new
-
-		missingItems.each do |item|
-
-			print "Inseting "
-			puts item[0]
-
-			itemJSON << downloader.getItemJSON(item[0])
-
-			#dbhandeler.insertItem(item[0], nameAndJSON[0], nameAndJSON[1])
-
-		end
-
-		dbhandeler.insertMissingItems(missingItems,itemJSON)
 
 
 	when "0"
