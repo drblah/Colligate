a8826a5c523660b22ba4da529df16c93806512fb
Sebastian Damsgaard <theblahblah2001@gmail.com>
2014-10-25 19:12:44 +0200

    Testing out a new worker class

---
+++ Worker.rb
@@ -0,0 +1,26 @@
+class Worker
+
+	def initialize
+		@jobList = []
+		$status = "run"
+	end
+
+	def addJob
+
+		@jobList << Thread.new { yield }
+		
+	end
+
+	def stopJobs
+
+		$status = "stop"
+
+		@jobList.each do |job|
+
+			job.join
+
+		end
+
+	end
+
+end
