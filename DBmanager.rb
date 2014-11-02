de0fc2244a838fb98908ab1af985aeee348ed93e
Sebastian Damsgaard <theblahblah2001@gmail.com>
2014-11-02 18:15:20 +0100

    Removed "hard coded" file name
    The new worker class should now work with multiple realms.

--- DBmanager.rb
+++ DBmanager.rb
@@ -109,7 +109,7 @@ class DBmanager
 
 		begin
 			
-			f = File.read("eu.argent-dawn.json", :mode => 'r:utf-8') # Open file as UTF-8
+			f = File.read("#{@region}.#{@realm}.json", :mode => 'r:utf-8') # Open file as UTF-8
 
 			f = f.lines.to_a[3..-1].join # Remove 3 first lines
 			f = f.gsub!( /\r\n?/, "\n" ) # Replace windows line endings with unix ( CRFL to FL )
