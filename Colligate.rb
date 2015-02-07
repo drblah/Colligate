25daa2e5cbcbde38dcf057cafbd3cebb77b77c03
Sebastian Damsgaard <theblahblah2001@gmail.com>
2015-02-07 19:23:24 +0100

    PostgreSQL to MySQL
    This will be the last commit for this branch for now.
    Playing with PostgreSQL have been very interesting but I have just not
    been able to make it perform as well as MySQL.
    One good thing that came out of this experiment was that I started using
    Sequel, so now I don't have to use mysql2 gem directly. Yay!

--- Colligate.rb
+++ Colligate.rb
@@ -51,44 +51,6 @@ realms.each do |r|
 
 end
 
-
-# Worker for item name resolve
-#worker.addJob {
-#
-#	while $status != "stop"
-#
-#		realms.each do |r|
-#
-#			downloader = Downloader.new(r["region"], r["realm"], r["locale"], @apikey)
-#			dbhandeler = DBmanager.new(r["region"], r["realm"])
-#
-#			missingItems = dbhandeler.itemsNotInDB
-#
-#			missingItems.each do |item|
-#
-#				iJSON = downloader.getItemJSON(item)
-#
-#				dbhandeler.insertItem(item, iJSON[0], iJSON[1])
-#
-#			end
-#
-#		end
-#
-#		puts "Sleeping item resolver..."
-#
-#		(0..99).each do
-#
-#			sleep(3)
-#
-#			Thread.exit if $status == "stop"
-#
-#		end
-#
-#
-#	end
-#}
-
-
 while true
 	
 	realms.each do |r|
@@ -210,86 +172,6 @@ while true
 
 
 end
-
-
-
-
-
-
-
-#realms.each do |r|
-#
-#	worker.addJob {
-#
-#		downloader = Downloader.new(r["region"], r["realm"], r["locale"], @apikey)
-#		dbhandeler = DBmanager.new(r["region"], r["realm"])
-#
-#		lastModified = 0
-#		oldLastModified = 0
-#		
-#		while $status != "stop"
-#
-#			puts "Checking for new data. #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
-#			log.info "Checking for new data. #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
-#
-#			dataInfo = downloader.getauctionURL
-#
-#			if dataInfo != nil
-#				lastModified = dataInfo[1]	
-#			end
-#
-#			if lastModified > oldLastModified
-#				@tokens.pop
-#				puts "New data is available. Beginning work..."
-#				log.info "New data is available. Beginning work..."
-#
-#				oldLastModified = lastModified
-#				
-#				success = downloader.downloadAuctionJSON(dataInfo[0])
-#
-#				if success
-#					
-#					success = dbhandeler.writeAuctionsToDB(dbhandeler.readAuctionJSONFile,lastModified)
-#
-#				end
-#				
-#				if success
-#				
-#					success = dbhandeler.moveoldtolog(lastModified)
-#
-#				end
-#
-#				if success
-#					
-#					dbhandeler.deleteold(lastModified)
-#
-#				end
-#
-#				
-#
-#				@tokens << 1
-#			else
-#
-#				puts "Nothing new yet."
-#				log.info "Nothing new yet."
-#
-#			end
-#
-#			puts "Sleeping..."
-#
-#			(0..99).each do
-#
-#				sleep(3)
-#
-#				Thread.exit if $status == "stop"
-#
-#			end
-#
-#			
-#
-#		end
-#	}		
-#end
 	
 while true
 
